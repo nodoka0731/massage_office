9f4b0eb024e0a272986dcb078f3aaae4ef31b338
nodoka0731 <rio0625@nifty.com>
2021-02-14 10:46:48 +0900

    firebaseを追加

--- site/app.js
+++ site/app.js
@@ -1,9 +1,22 @@
-const express = require('express');
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
 
-const app = express();
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
 
-app.get('/',(req,res) => {
-    res.render('top.ejs');
-});
-
-app.listen(3000);
+// TODO: Replace the following with your app's Firebase project configuration
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+var firebaseConfig = {
+    apiKey: "AIzaSyBo0AsnNlX9hvVt4RcKLZC8PoEDJTYiDpg",
+  authDomain: "massage-office.firebaseapp.com",
+  projectId: "massage-office",
+  storageBucket: "massage-office.appspot.com",
+  messagingSenderId: "868403640666",
+  appId: "1:868403640666:web:de494e934933866f305b63",
+  measurementId: "G-XLBTN4E6SK"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
